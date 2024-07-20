9df83d20122333f8e8fadb662faa78f7f9e4e54e
sudhakant-solutelabs <sudhakant.p@solutelabs.com>
2024-07-20 18:36:09 +0530

    Response From HTML File

--- 02-Basic-NodeJs-Server/app.js
+++ 02-Basic-NodeJs-Server/app.js
@@ -1,10 +1,21 @@
 const http = require('http');
 // const https = require('https');
+const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World!');
-    res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', (error, data) => {
+        if(error){
+            res.writeHead(404);
+            res.write('Error: File not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+    // res.write('Hello World!');
+    // res.end();
 });
 
 server.listen(port, (err) => err ? console.log(err) : console.log(`Listening on port ${port}`));
