2fca3a07dca46fbfd159415d51b08ed34370298f
sudhakant-solutelabs <sudhakant.p@solutelabs.com>
2024-07-25 13:50:10 +0530

    Setting Up views Path

--- 04-URL-Shortener-MongoDB/server.js
+++ 04-URL-Shortener-MongoDB/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const {PORT, DB_URL} = require('../core/environment/index');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const uri = DB_URL;
@@ -14,10 +15,11 @@ mongoose.connect(uri)
 
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('.\\04-URL-Shortener-MongoDB\\views\\index');
+    res.render('index');
 })
 
 app.listen(PORT, () => console.log(`Server is Listening on PORT ${PORT}`));
