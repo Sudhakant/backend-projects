035ab132074efaad8c5ef8689999807ea62378e9
sudhakant-solutelabs <sudhakant.p@solutelabs.com>
2024-07-25 23:03:50 +0530

    ShortUrl Model

---
+++ 04-URL-Shortener-MongoDB/models/shortUrl.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+// shortId.generate();
+
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+        type: String,
+        required: true
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    },
+});
