a7c567e6fc39f4c1f395b8c3494063a27d095d24
sudhakant-solutelabs <sudhakant.p@solutelabs.com>
2024-07-26 01:20:36 +0530

    POST Route Creation

--- 04-URL-Shortener-MongoDB/server.js
+++ 04-URL-Shortener-MongoDB/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const ShortUrl = require('./models/shortUrl');
 const {PORT, DB_URL} = require('../core/environment/index');
 const path = require('path');
 
@@ -14,17 +15,25 @@ mongoose.connect(uri)
 });
 
 
+// Converting the received data from JSON to JS Object
 app.use(express.json());
+// Converting the form data to JS Object
+app.use(express.urlencoded({extended: false}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
 });
 
-app.post('/shortUrls', (req, res) => {
-    
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({full: req.body.fullUrl})
+    // .then(shortUrl => {
+    //     res.send(shortUrl);
+    // });
+    res.redirect('/');
 })
 
 app.listen(PORT, () => console.log(`Server is Listening on PORT ${PORT}`));
