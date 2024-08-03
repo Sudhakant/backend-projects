9efd8633c3f864db2453bc23c75fe6a9fc4fc33f
sudhakant-solutelabs <sudhakant.p@solutelabs.com>
2024-08-04 03:48:41 +0530

    Creating Dummy Users at the very early start of the Database with No Existing Users

--- 06-Paginated-API-MongoDB/server.js
+++ 06-Paginated-API-MongoDB/server.js
@@ -12,6 +12,28 @@ mongoose.connect(DB_URL)
     console.log(err);
 });
 
+const db = mongoose.connection;
+db.once('open', async () => {
+    if(await User.countDocuments().exec() > 0) return
+
+    Promise.all([
+        User.create({name: 'User 1'}),
+        User.create({name: 'User 2'}),
+        User.create({name: 'User 3'}),
+        User.create({name: 'User 4'}),
+        User.create({name: 'User 5'}),
+        User.create({name: 'User 6'}),
+        User.create({name: 'User 7'}),
+        User.create({name: 'User 8'}),
+        User.create({name: 'User 9'}),
+        User.create({name: 'User 10'}),
+        User.create({name: 'User 11'}),
+        User.create({name: 'User 12'}),
+        User.create({name: 'User 13'}),
+        User.create({name: 'User 14'}),
+    ]).then(() => console.log('All Users Created'));
+})
+
 
 app.get('/users', paginatedResults(User), (req, res)=>{
     const results = res.paginatedResults;
